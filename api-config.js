167ee89059701864daf7e642ea994a282dc3c2c8
Jai Gupta <jaigu@siklot.com>
2026-04-01 15:05:01 +0530

    Fix offline issue when opening local HTML files

--- api-config.js
+++ api-config.js
@@ -10,7 +10,7 @@ const BACKEND_URL = "https://gp.loftix.host";
 
 window.getApiUrl = function(path) {
     // If you are testing safely on your computer, always bounce it to port 3000
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:') {
         return 'http://localhost:3000' + path;
     }
     
